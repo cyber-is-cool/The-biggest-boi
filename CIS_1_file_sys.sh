febd1598369da82074994318e00e3f00f614f6ed
cyber-is-cool <120389483+cyber-is-cool@users.noreply.github.com>
2023-09-11 11:27:30 -0500

    Create CIS_1_file_sys.sh

---
+++ CIS_1_file_sys.sh
@@ -0,0 +1,40 @@
+#! /bin/bash
+
+if [[ $(/usr/bin/id -u) -ne 0 ]]; then
+    echo "Not running as root"
+    exit
+fi
+#1.1
+clear
+"""
+/bin/bash CIS_1_file_sys_rem.sh cramfs fs
+read
+clear
+/bin/bash CIS_1_file_sys_rem.sh freevxfs fs 
+read
+clear
+/bin/bash CIS_1_file_sys_rem.sh jffs2 fs
+read
+clear
+/bin/bash CIS_1_file_sys_rem.sh hfs fs
+read
+clear
+/bin/bash CIS_1_file_sys_rem.sh hfsplus fs
+read
+clear
+/bin/bash CIS_1_file_sys_rem.sh squashfs fs 
+read
+clear
+/bin/bash CIS_1_file_sys_rem.sh udf fs 
+read
+clear
+"""
+clear
+#1.2
+
+if [[ $(findmnt -nk /tmp) ]]; then
+	echo "/tmp is mounted"
+else
+	echo "/tmp is not mounted"
+	
+fi
