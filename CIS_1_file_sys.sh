a5f1a5837b47add1ca68d75eea89e23da2dbf421
cyber-is-cool <120389483+cyber-is-cool@users.noreply.github.com>
2023-09-13 09:21:03 -0500

    Update CIS_1_file_sys.sh

--- CIS_1_file_sys.sh
+++ CIS_1_file_sys.sh
@@ -6,7 +6,7 @@ if [[ $(/usr/bin/id -u) -ne 0 ]]; then
 fi
 #1.1
 clear
-"""
+
 /bin/bash CIS_1_file_sys_rem.sh cramfs fs
 read
 clear
@@ -28,16 +28,25 @@ clear
 /bin/bash CIS_1_file_sys_rem.sh udf fs 
 read
 clear
-"""
-clear
-#1.2
-if [[ $(findmnt -nk /tmp | grep "1777,strictatime,size=2G,noexec,nodev,nosuid") ]]; then
-	echo "/tmp is mounted"
-else
-	echo "/tmp is not mounted"
-	cp -v /usr/share/systemd/tmp.mount /etc/systemd/system
-	sed -i "/^Options=mode/s/=.*$/=1777,strictatime,size=2G,noexec,nodev,nosuid/" /etc/systemd/system/tmp.mount
-	#systemctl daemon-reload
-	#systemctl --now enable tmp.mount
-fi
 
+clear
+#/tmp
+cp -v /usr/share/systemd/tmp.mount /etc/systemd/system/
+systemctl daemon-reload | systemctl --now enable tmp.mount
+echo "tmpfs           /tmp            tmpfs    defaults,rw,nosuid,nodev,noexec,relatime,rw,nosuid,nodev,noexec,relatime  0  0" >> /etc/fstab
+
+#/dev/shm partition
+echo "tmpfs           /dev/shm            tmpfs    defaults,noexec,nodev,nosuid  0  0" >> /etc/fstab
+
+####var/tmp partition
+#parted /dev/nvme0n1p5
+#mount -t ext4 /dev/sda3 /var/tmp
+echo "ext4           /var/tmp            ext4   defaults,nosuid,nodev,noexec  0  0" >> /etc/fstab
+
+echo "ext4           /home            ext4   defaults,nodev  0  0" >> /etc/fstab
+
+df --local -P | awk '{if (NR!=1) print $6}' | xargs -I '{}' find '{}' -xdev -type d \( -perm -0002 -a ! -perm -1000 \) 2>/dev/null | xargs -I '{}' chmod a+t '{}'
+#automounting USB 1.1.9
+apt purge autofs
+# 1.1.10
+/bin/bash CIS_1_file_sys_rem.sh usb-storage drivers 
