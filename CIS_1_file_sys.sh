1568bbd21c6b85d2dd34282d1da11cd72015ccc4
cyber-is-cool <120389483+cyber-is-cool@users.noreply.github.com>
2023-09-19 11:16:01 -0500

    Update CIS_1_file_sys.sh

--- CIS_1_file_sys.sh
+++ CIS_1_file_sys.sh
@@ -6,7 +6,9 @@ if [[ $(/usr/bin/id -u) -ne 0 ]]; then
 fi
 #1.1
 clear
-'''
+
+if [ 1 == 2 ]; then
+
 /bin/bash CIS_1_file_sys_rem.sh cramfs fs
 read
 clear
@@ -28,7 +30,7 @@ clear
 /bin/bash CIS_1_file_sys_rem.sh udf fs 
 read
 clear
-
+'''
 clear
 #/tmp
 cp -v /usr/share/systemd/tmp.mount /etc/systemd/system/
@@ -45,7 +47,7 @@ echo "ext4           /var/tmp            ext4   defaults,nosuid,nodev,noexec  0 
 
 echo "ext4           /home            ext4   defaults,nodev  0  0" >> /etc/fstab
 
-
+'''
 #automounting USB 1.1.9
 apt purge autofs
 read
@@ -54,7 +56,7 @@ clear
 /bin/bash CIS_1_file_sys_rem.sh usb-storage drivers 
 read
 clear
-'''
+
 #1.2
 apt install aide aide-common -y
 clear
@@ -89,12 +91,122 @@ chmod 0644 /etc/systemd/system/aidecheck.*
 systemctl daemon-reload
 systemctl enable aidecheck.service
 systemctl --now enable aidecheck.timer
-
+read
 # 1.3
 apt upgrade
 # 1.3
+read
+clear
 apt-cache policy
+read
+clear
 apt-key list
+read
+
+clear
+grep -q "^set superusers" /boot/grub/grub.cfg
+if [ $? -eq 0 ]; then
+	grep -q "^password" /boot/grub/grub.cfg
+	if [ $? -eq 0 ]; then
+		echo grub2 username and password is set
+	else
+		echo ---GRUB2 MISCONFIGURED---
+		echo ---PASSWORD-NOT-SET---
+		
+	fi
+else
+	grep -q "^password" /boot/grub/grub.cfg
+	if [ $? -eq 0 ]; then
+		echo ---GRUB2 MISCONFIGURED---
+		echo ---PASSWORD-SET-NO-USER-
+	fi
+fi
+
+grep -q "^set superusers" /boot/grub/grub.cfg
+if [ $? -eq 1 ]; then
+	grep -q "^password" /boot/grub/grub.cfg
+	if [ $? -eq 1 ]; then
+	echo grub2 not configured right
+fi	
+fi
+
+read
+clear
+
+grub_test=$(stat -Lc "Access: (%#a/%A) Uid: ( %u/ %U) Gid: ( %g/ %G)" /boot/grub/grub.cfg)
+
+if [ "$grub_test" == "Access: (0600/-rw-------) Uid: ( 0/ root) Gid: ( 0/ root)" ] || [ "$grub_test" == "Access: (0400/-r--------) Uid: ( 0/ root) Gid: ( 0/ root)" ]; then
+	echo "Grub config file is set correctly"
+else
+	echo "Grub config file is NOT set correctly"
+	chown root:root /boot/grub/grub.cfg
+	chmod u-x,go-rwx /boot/grub/grub.cfg
+fi
+
+read
+clear
+
+root=$(passwd --status root)
+if [[ "$root" == *"NP"* ]]; then
+	echo No password on root account
+	passwd root
+else
+	echo password on root good
+fi
+
+
+# 1.5
+
+prelink -ua
+apt purge prelink
+read
+clear
+
+touch /etc/sysctl.d/60-kernel_sysctl.conf &&
+echo "kernel.randomize_va_space = 2" >> /etc/sysctl.d/60-kernel_sysctl.conf
+sysctl -w kernel.randomize_va_space=2
+echo "kernel.yama.ptrace_scope = 1" >> /etc/sysctl.d/60-kernel_sysctl.conf
+sysctl -w kernel.yama.ptrace_scope=1
+echo "fs.suid_dumpable = 0" >> /etc/sysctl.d/60-kernel_sysctl.conf
+sysctl -w fs.suid_dumpable=0
+
+
+service procps force-reload
+
+
+systemctl stop apport.service
+systemctl --now disable apport.service
+apt purge apport
+
+read 
+clear
+cat /etc/systemd/coredump.conf
+echo $?
+if [ $? -eq 0 ]; then {
+	echo "Storage=none" >> /etc/systemd/coredump.conf
+	echo "ProcessSizeMax=0" >> /etc/systemd/coredump.conf
+	systemctl daemon-reload
+}
+fi
+
+
+fi ##
+
+apt install apparmor apparmor-utils
+clear
+out=$(dpkg-query -W -f='${binary:Package}\t${Status}\t${db:Status-Status}\n'apparmor apparmor-utils)
+if [[ "$out" == *"not-installed"* ]]; then {
+	echo apparmor not installed good
+	apt install apparmor apparmor-utils
+}
+else {
+	echo apparmor is good
+}
+fi
+
+sed -i 's/GRUB_CMDLINE_LINUX=/GRUB_CMDLINE_LINUX="apparmor=1 security=apparmor"/'
+
+
 
 
 
