ec3994ffa282d98228d648427c250719c2ec73eb
cyber-is-cool <120389483+cyber-is-cool@users.noreply.github.com>
2023-09-26 11:15:48 -0500

    Update The_biggest_boi.sh

--- The_biggest_boi.sh
+++ The_biggest_boi.sh
@@ -1,1 +1,85 @@
+#! /bin/bash
+
+apt_fix (){
+# set sources
+ubuntu_sources="
+deb http://us.archive.ubuntu.com/ubuntu/ CHANGEME main restricted\n
+deb http://us.archive.ubuntu.com/ubuntu/ CHANGEME-updates main restricted\n
+deb http://us.archive.ubuntu.com/ubuntu/ CHANGEME universe\n
+deb http://us.archive.ubuntu.com/ubuntu/ CHANGEME-updates universe\n
+deb http://us.archive.ubuntu.com/ubuntu/ CHANGEME multiverse\n
+deb http://us.archive.ubuntu.com/ubuntu/ CHANGEME-updates multiverse\n
+deb http://us.archive.ubuntu.com/ubuntu/ CHANGEME-backports main restricted universe multiverse\n
+deb http://security.ubuntu.com/ubuntu CHANGEME-security main restricted\n
+deb http://security.ubuntu.com/ubuntu CHANGEME-security universe\n
+deb http://security.ubuntu.com/ubuntu CHANGEME-security multiverse\n
+
+deb-src http://us.archive.ubuntu.com/ubuntu/ CHANGEME main restricted\n
+deb-src http://us.archive.ubuntu.com/ubuntu/ CHANGEME-updates main restricted\n
+deb-src http://us.archive.ubuntu.com/ubuntu/ CHANGEME universe\n
+deb-src http://us.archive.ubuntu.com/ubuntu/ CHANGEME-updates universe\n
+deb-src http://us.archive.ubuntu.com/ubuntu/ CHANGEME multiverse\n
+deb-src http://us.archive.ubuntu.com/ubuntu/ CHANGEME-updates multiverse\n
+deb-src http://us.archive.ubuntu.com/ubuntu/ CHANGEME-backports main restricted universe multiverse\n
+deb-src http://security.ubuntu.com/ubuntu CHANGEME-security main restricted\n
+deb-src http://security.ubuntu.com/ubuntu CHANGEME-security universe\n
+deb-src http://security.ubuntu.com/ubuntu CHANGEME-security multiverse\n
+"
+sudo cp -r /etc/apt/sources.list* backup/apt/ 
+sudo rm -f /etc/apt/sources.list
+
+echo -e $ubuntu_sources | sed "s/ deb/deb/g; s/CHANGEME/${CODENAME}/g" | sudo tee /etc/apt/sources.list > /dev/null
+
+sudo $APT install -y unattended-upgrades apt-listchanges
+    
+# auto updates
+echo 'APT::Periodic::Update-Package-Lists "1";'             | sudo tee /etc/apt/apt.conf.d/10periodic > /dev/null
+echo 'APT::Periodic::Download-Upgradeable-Packages "1";'    | sudo tee -a /etc/apt/apt.conf.d/10periodic > /dev/null
+echo 'APT::Periodic::Unattended-Upgrade "1";'               | sudo tee -a /etc/apt/apt.conf.d/10periodic > /dev/null
+echo 'APT::Periodic::AutocleanInterval "7";'                | sudo tee -a /etc/apt/apt.conf.d/10periodic > /dev/null
+
+echo 'APT::Periodic::Update-Package-Lists "1";'             | sudo tee /etc/apt/apt.conf.d/20auto-upgrades > /dev/null
+echo 'APT::Periodic::Download-Upgradeable-Packages "1";'    | sudo tee -a /etc/apt/apt.conf.d/20auto-upgrades > /dev/null
+echo 'APT::Periodic::Unattended-Upgrade "1";'               | sudo tee -a /etc/apt/apt.conf.d/20auto-upgrades > /dev/null
+echo 'APT::Periodic::AutocleanInterval "7";'                | sudo tee -a /etc/apt/apt.conf.d/20auto-upgrades > /dev/null
+
+}
+
+
+main (){
+
+
+  mkdir -p backup/users
+  mkdir -p backup/pam
+  mkdir -p backup/apt
+    
+  mkdir -p backup/services
+  mkdir -p backup/services/crons
+  mkdir -p backup/services/startup
+
+  mkdir -p backup/networking
+  mkdir -p backup/system
+  mkdir -p backup/malware
+
+  mkdir -p backup/misc
+  mkdir -p backup/misc/media
+    
+  echo install apt-fast
+  sudo apt install -y software-properties-common
+  sudo add-apt-repository ppa:apt-fast/stable -y
+
+  
+  apt_fix
+  
+  sudo apt-get update
+  sudo DEBIAN_FRONTEND=noninteractive apt install -y apt-fast && APT=apt-fast
+
+  
+  
+
+
+}
+
+
+
 
